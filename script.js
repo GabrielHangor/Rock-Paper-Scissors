ac1fb549f735a17e9862ec53150c9467f2bb4de3
HangorG <gaba93@yandex.ru>
2020-12-27 01:54:08 +0300

    Compare user's input and computer's random choice

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+const playerSelection = prompt('Enter your choice: "Rock", "Paper" or "Scissors"').toLowerCase();
+const computerSelection = computerPlay();
 
-// Reutrns Rock, Paper or Scissors value randomly
+const rules = {
+  rock: { name: "rock", defeats: ["scissors"] },
+  paper: { name: "paper", defeats: ["rock"] },
+  scissors: { name: "scissors", defeats: ["paper"] },
+};
+
+// Returns Rock, Paper or Scissors value randomly
 function computerPlay() {
-  const computerChoices = ["Rock", "Paper", "Scissors"];
+  const computerChoices = ["rock", "paper", "scissors"];
   const randomChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
   return randomChoice;
 }
 
 
+// Compare user's input with a computer's random choice and returns some text
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It is a tie!";
+  } else {
+    const choice = rules[playerSelection];
+    if (choice.defeats.includes(computerSelection)) {
+      return `${playerSelection} beats ${computerSelection}. You won!`;
+    } else {
+      return `You lost! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+}
+
+playRound(playerSelection, computerSelection);
